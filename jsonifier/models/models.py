f58c7447cadb64da6b5b7ac435664740ee6b599d
Simone Orsi <simahawk@gmail.com>
2025-02-21 10:29:20 +0100

    jsonifier: fix exception warning

--- jsonifier/models/models.py
+++ jsonifier/models/models.py
@@ -135,7 +135,7 @@ class Base(models.AbstractModel):
         function = field_dict["function"]
         try:
             return self._function_value(rec, function, field_name)
-        except UserError:
+        except UserError as err:
             if strict:
                 raise
             if not tools.config["test_enable"]:
@@ -143,7 +143,7 @@ class Base(models.AbstractModel):
                     "%(model)s.%(func)s not available",
                     {"model": self._name, "func": str(function)},
                 )
-                raise SwallableException()
+                raise SwallableException() from err
 
     def _jsonify_record_handle_subparser(self, rec, field_dict, strict, subparser):
         field_name = field_dict["name"]
